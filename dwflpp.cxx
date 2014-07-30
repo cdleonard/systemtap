a6e9e9ca5471b7e685e8281a413d4dd5a63d3122
Crestez Dan Leonard <lcrestez@ixiacom.com>
2014-07-30 20:37:52 +0300

    loc2c: Add Dwarf pointer to location_context

--- dwflpp.cxx
+++ dwflpp.cxx
@@ -3234,7 +3234,7 @@ dwflpp::translate_location(struct obstack *pool,
       l2c_ctx.pc = pc;
       l2c_ctx.die = die;
       *tail = c_translate_constant (pool, &loc2c_error, this,
-				    &loc2c_emit_address, 0, pc, attr);
+				    &loc2c_emit_address, 0, pc, attr, module_dwarf);
       return *tail;
     }
 
@@ -3314,7 +3314,7 @@ dwflpp::translate_location(struct obstack *pool,
   return c_translate_location (pool, &loc2c_error, this,
                                &loc2c_emit_address,
                                1, 0 /* PR9768 */,
-                               pc, attr, expr, len, tail, fb_attr, cfa_ops);
+                               pc, attr, expr, len, tail, fb_attr, cfa_ops, module_dwarf);
 }
 
 
@@ -3926,7 +3926,7 @@ dwflpp::literal_stmt_for_local (vector<Dwarf_Die>& scopes,
 	  head = c_translate_location (&pool, &loc2c_error, this,
 				       &loc2c_emit_address,
 				       1, 0, pc,
-				       NULL, &addr_loc, 1, &tail, NULL, NULL);
+				       NULL, &addr_loc, 1, &tail, NULL, NULL, module_dwarf);
 	}
       else
 	{
@@ -4030,7 +4030,7 @@ dwflpp::literal_stmt_for_return (Dwarf_Die *scope_die,
                                                  &loc2c_emit_address,
                                                  1, 0 /* PR9768 */,
                                                  pc, NULL, locops, nlocops,
-                                                 &tail, NULL, NULL);
+                                                 &tail, NULL, NULL, module_dwarf);
 
   /* Translate the ->bar->baz[NN] parts. */
 
@@ -4092,7 +4092,7 @@ dwflpp::literal_stmt_for_pointer (Dwarf_Die *start_typedie,
   l2c_ctx.die = start_typedie;
   struct location *head = c_translate_argument (&pool, &loc2c_error, this,
                                                 &loc2c_emit_address,
-                                                1, "STAP_ARG_pointer");
+                                                1, "STAP_ARG_pointer", module_dwarf);
   struct location *tail = head;
 
   /* Translate the ->bar->baz[NN] parts. */
